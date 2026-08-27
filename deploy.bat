c9ca481b216bc38d1324751c1b538d0ab59c2261
erotic-lion <erotiquelion@gmail.com>
2026-08-27 10:01:04 +0900

    Update site source

--- deploy.bat
+++ deploy.bat
@@ -19,7 +19,7 @@ git commit -m "Update site source"
 git push origin main
 echo.
 echo [3/3] Deploying dist to gh-pages branch...
-call npx gh-pages -d dist
+call npx gh-pages -d dist --repo=https://github.com/k28ueno/BTS.git
 if %errorlevel% neq 0 (
   color 0C
   echo Deploy failed!

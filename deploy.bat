ee66267374ec8258315698bb9f0eb97021942dd5
erotic-lion <erotiquelion@gmail.com>
2026-08-27 09:47:45 +0900

    Update site source

---
+++ deploy.bat
@@ -0,0 +1,22 @@
+@echo off
+color 0A
+========================================
+ 1-Click Deploying to GitHub Pages...
+========================================
+
+[1/3] Building latest site...
+
+> bts@0.0.0 build
+> vite build
+
+[36mvite v5.4.21 [32mbuilding for production...[36m[39m
+transforming...
+[32m✓[39m 31 modules transformed.
+rendering chunks...
+computing gzip size...
+[2mdist/[22m[32mindex.html                 [39m[1m[2m  0.47 kB[22m[1m[22m[2m │ gzip:  0.30 kB[22m
+[2mdist/[22m[35massets/index-D-c0nroh.css  [39m[1m[2m 10.50 kB[22m[1m[22m[2m │ gzip:  2.71 kB[22m
+[2mdist/[22m[36massets/index-1WZIvuRn.js   [39m[1m[2m148.77 kB[22m[1m[22m[2m │ gzip: 47.93 kB[22m
+[32m✓ built in 24.59s[39m
+
+[2/3] Saving source code to main branch...
